09f813f62d721757923e718d8396e944bb178d06
abaranya <abaranya@gmail.com>
2015-05-16 00:59:53 +0200

    parsing regexps

--- fileparsing/probfile.py
+++ fileparsing/probfile.py
@@ -21,8 +21,27 @@ class ProbFile:
     def get_filename(self):
         return self.filename
 
-    def read_record(self,file):
-        return []  # todo: add proper behaviour
+    def read_next_line(self, file):
+        #  todo: add use cases for different endline chars
+
+        return line
+
+    def read_records(self,file):
+        #  check endline params
+        if not self._specs[0].byline:
+            #  todo: implement different endline char behavior
+            return []
+        records = []
+
+        for line in file:
+            records.append(read_next_line(file))
+
+        values = []
+
+        if self._specs[0].format == 'regexp':
+            values = parse_regexp(self._specs[0].regexp, records)
+
+        return values  # todo: add proper behaviour
 
     def read_file(self):
         if self.filename is not None:
@@ -30,7 +49,7 @@ class ProbFile:
                 record_list = []
 
                 with open(self.filename) as file:
-                    record_list.append(self.read_record(file))
+                    record_list.append(self.read_records(file))
 
             except (IOError, OSError, Failure) as e:
                 debug("An terrible exception occurred")
